c689ba5d70b52ab77b5c3723622f6b78f2edde96
Ensign <ensign@workstation.liddev.com>
2015-10-21 10:39:13 -0600

    Changed playagain.html

--- Projects/app/controllers/playagain_controller.rb
+++ Projects/app/controllers/playagain_controller.rb
@@ -2,7 +2,4 @@ class PlayagainController < ApplicationController
   def index
     # all calls to the database must go through the bus class
   end
-  def show
-    # all calls to the database must go through the bus class
-  end
 end
