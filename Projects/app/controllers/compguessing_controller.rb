52f4c2ed6e50a0ad99fb193e3e9bf6c823bf66ac
cshibe <cshibe@uccs.edu>
2015-10-23 15:50:26 -0600

    fixed compguessing issues

--- Projects/app/controllers/compguessing_controller.rb
+++ Projects/app/controllers/compguessing_controller.rb
@@ -1,9 +1,34 @@
 class CompguessingController < ApplicationController
   def index
+    if params[:node_number] == '0'
+      @category = params[:category]
+      @question = Bus.get_computer_guess_info(@category, 1)
+      @node_number = 1
+      session[:node_number] = @node_number
+      redirect_to :action=>"index", :controller=> "compguessing",  :category=>"#{@category}", :node_number => @node_number
+    else
+      if (params[:button_clicked].to_s == "yes") && (session[:node_number].to_s == params[:node_number].to_s)
+        @category = params[:category]
+        @node_number = params[:node_number]
+        @node_number = 2*Integer(@node_number)
+        redirect_to :action=>"index", :controller=> "compguessing",  :category=>"#{@category}", :node_number => @node_number
+      elsif (params[:button_clicked] == "no") && (session[:node_number].to_s == params[:node_number].to_s)
+             @category = params[:category]
+             @node_number = params[:node_number]
+             @node_number = 2*Integer(@node_number) + 1
+             redirect_to :action=>"index", :controller=> "compguessing",  :category=>"#{@category}", :node_number => @node_number
+      end
+      @node_number = params[:node_number]
+      session[:node_number] = @node_number
+      @category = params[:category]
+      @question = Bus.get_computer_guess_info(@category, @node_number)
+      if @question.what_am_i.to_s == 'Answer'
+        redirect_to :action=>"index", :controller=>"playagain", :answer => @question.answer_or_question.to_s
+      end
+    end
+
+
     # all calls to the database must go through the bus class
-    @category = params[:category]
-    @node_number = params[:node_number]
-    @question = Bus.get_computer_guess_info(@category, @node_number)
     #if @question.what_am_i.to_s == "Answer"
     # return RedirectToAction("index", :question => @question)
     #else
--- Projects/app/controllers/playagain_controller.rb
+++ Projects/app/controllers/playagain_controller.rb
@@ -1,5 +1,5 @@
 class PlayagainController < ApplicationController
   def index
-    # all calls to the database must go through the bus class
+    @answer = params[:answer]
   end
 end
