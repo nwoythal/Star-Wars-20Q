28f7f4d007f98990ad41c295d54415ec048ca400
David Stout <dstout2@uccs.edu>
2015-11-13 19:44:46 -0700

    updated rules

--- Projects/spec/models/bus_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-include ProjectsHelper
-
-describe Starwar do
-  it 'can find the first question to start the game' do
-    expect(Starwar.first.node_number).to eq(1)
-  end
-end
