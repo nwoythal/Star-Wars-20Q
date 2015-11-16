9e98481cf75f9fb1d40538b9df7a66a90a6ab68c
cshibe <cshibe@uccs.edu>
2015-11-16 10:06:50 -0700

    adding Jedi data

--- Projects/db/seeds.rb
+++ Projects/db/seeds.rb
@@ -20,10 +20,6 @@ rebels = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number =>
 
 universes = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number => 1}]
 
-jedis = [{:what_am_i => 'Question', :answer_or_question => 'Are you a Male?', :node_number => 1},
-         {:what_am_i => 'Answer', :answer_or_question => 'Guy', :node_number => 2},
-         {:what_am_i => 'Answer', :answer_or_question => 'Girl', :node_number => 3}]
-
 siths = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number => 1}]
 
 senators = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number => 1}]
@@ -35,6 +31,62 @@ clones = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number =>
 machines = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number => 1}]
 
 moons = [{:what_am_i => 'Answer', :answer_or_question => 'n/a', :node_number => 1}]
+
+jedis = [{:what_am_i => 'Question', :answer_or_question => 'Is this Jedi a Male?', :node_number => 1},
+         {:what_am_i => 'Question', :answer_or_question => 'Did he fight in the Clone Wars?', :node_number => 2},
+         {:what_am_i => 'Question', :answer_or_question => 'Does this Jedi have hair on his head?', :node_number => 4},
+         {:what_am_i => 'Question', :answer_or_question => 'Is this Jedi small and have green skin?', :node_number => 8},
+         {:what_am_i => 'Answer', :answer_or_question => 'Yaddle', :node_number => 16},
+         {:what_am_i => 'Question', :answer_or_question => 'Does this Jedi have one eye?', :node_number => 17},
+         {:what_am_i => 'Answer', :answer_or_question => 'Even Piell', :node_number => 34},
+         {:what_am_i => 'Question', :answer_or_question => 'Is he of the species Thisspiasian?', :node_number => 35},
+         {:what_am_i => 'Question', :answer_or_question => 'Oppo Rancisis', :node_number => 70},
+         {:what_am_i => 'Question', :answer_or_question => 'Was he killed by Darth Varder?', :node_number => 71},
+         {:what_am_i => 'Answer', :answer_or_question => 'Obi-Wan Kenobi', :node_number => 142},
+         {:what_am_i => 'Answer', :answer_or_question => 'Anakin Skywalker', :node_number => 143},
+         {:what_am_i => 'Question', :answer_or_question => 'Is he of the species Kel Dor?', :node_number => 9},
+         {:what_am_i => 'Question', :answer_or_question => 'Is he the keeper of the Jedi Archives?', :node_number => 18},
+         {:what_am_i => 'Answer', :answer_or_question => 'Gnost Dural', :node_number => 36},
+         {:what_am_i => 'Answer', :answer_or_question => 'Plo Koon', :node_number => 37},
+         {:what_am_i => 'Question', :answer_or_question => 'Is his skin color Green?', :node_number => 19},
+         {:what_am_i => 'Question', :answer_or_question => 'Was he killed by Darth Sidious?', :node_number => 38},
+         {:what_am_i => 'Answer', :answer_or_question => 'Kit Fisto', :node_number => 76},
+         {:what_am_i => 'Answer', :answer_or_question => 'Yoda', :node_number => 77},
+         {:what_am_i => 'Question', :answer_or_question => 'Is his skin color Dark?', :node_number => 39},
+         {:what_am_i => 'Question', :answer_or_question => 'Does he use a purple Lightsaber?', :node_number => 78},
+         {:what_am_i => 'Answer', :answer_or_question => 'Mace Windu', :node_number => 156},
+         {:what_am_i => 'Question', :answer_or_question => 'Was he killed by a bounty hunter?', :node_number => 157},
+         {:what_am_i => 'Answer', :answer_or_question => 'Coleman Trebor', :node_number => 314},
+         {:what_am_i => 'Question', :answer_or_question => 'Did he master every form of lightsaber technic?', :node_number => 315},
+         {:what_am_i => 'Answer', :answer_or_question => 'Sora Bulq', :node_number => 630},
+         {:what_am_i => 'Answer', :answer_or_question => 'Agen Kolar', :node_number => 631},
+         {:what_am_i => 'Question', :answer_or_question => 'Was he killed by General Grievous?', :node_number => 79},
+         {:what_am_i => 'Answer', :answer_or_question => 'Pablo Jill', :node_number => 158},
+         {:what_am_i => 'Question', :answer_or_question => 'Was he killed in Order 66 by his Clone Troopers? ', :node_number => 159},
+         {:what_am_i => 'Answer', :answer_or_question => 'Ki Adi Mundi', :node_number => 318},
+         {:what_am_i => 'Question', :answer_or_question => 'Does he have a long neck?', :node_number => 319},
+         {:what_am_i => 'Answer', :answer_or_question => 'Yarael Poof', :node_number => 638},
+         {:what_am_i => 'Answer', :answer_or_question => 'Saesee Tiin', :node_number => 639},
+         {:what_am_i => 'Question', :answer_or_question => 'Is his father Darth Varder?', :node_number => 5},
+         {:what_am_i => 'Answer', :answer_or_question => 'Luke Skywalker', :node_number => 10},
+         {:what_am_i => 'Answer', :answer_or_question => 'Qui Gon Jinn', :node_number => 11},
+         {:what_am_i => 'Question', :answer_or_question => 'Is she of the species Togruta?', :node_number => 3},
+         {:what_am_i => 'Answer', :answer_or_question => 'Ahsoka Tano', :node_number => 12},
+         {:what_am_i => 'Answer', :answer_or_question => 'Shaak Ti', :node_number => 13},
+         {:what_am_i => 'Question', :answer_or_question => 'Is she of the species Mirialan', :node_number => 7},
+         {:what_am_i => 'Question', :answer_or_question => 'Was she killed in Order 66 by her Clone Troopers?', :node_number => 14},
+         {:what_am_i => 'Answer', :answer_or_question => 'Luminara Unduli', :node_number => 28},
+         {:what_am_i => 'Answer', :answer_or_question => 'Barris Offee', :node_number => 29},
+         {:what_am_i => 'Question', :answer_or_question => 'Was she in charge of the Jedi Archives?', :node_number => 15},
+         {:what_am_i => 'Answer', :answer_or_question => 'Jocasta Nu', :node_number => 30},
+         {:what_am_i => 'Question', :answer_or_question => 'Is her skin color Blue?', :node_number => 31},
+         {:what_am_i => 'Answer', :answer_or_question => 'Aayla Secura', :node_number => 62},
+         {:what_am_i => 'Question', :answer_or_question => 'Was she killed by the Nightbrothers?', :node_number => 63},
+         {:what_am_i => 'Answer', :answer_or_question => 'Adi Gallia', :node_number => 126},
+         {:what_am_i => 'Question', :answer_or_question => 'Was she considered one of the most enlightened Jedi?', :node_number => 127},
+         {:what_am_i => 'Answer', :answer_or_question => 'Depa Billaba', :node_number => 254},
+         {:what_am_i => 'Answer', :answer_or_question => 'Bultar Swan', :node_number => 255},
+         {:what_am_i => 'Question', :answer_or_question => 'Was she trained by Anakin Skywalker?', :node_number => 6}]
 
 questions = [{:what_am_i => 'Question', :answer_or_question => 'Are you a Person?', :node_number => 1},
              {:what_am_i => 'Question', :answer_or_question => 'Is it a Place?', :node_number => 2},
@@ -55,6 +107,11 @@ questions = [{:what_am_i => 'Question', :answer_or_question => 'Are you a Person
              {:what_am_i => 'Question', :answer_or_question => 'Is it a Machine?', :node_number => 17},
              {:what_am_i => 'Question', :answer_or_question => 'Is it larger than a Moon?', :node_number => 18},
 ]
+
+
+jedis.each do |task|
+  Jedi.create!(task)
+end
 
 persons.each do |task|
   Starthing.create!(task)
@@ -100,9 +157,6 @@ universes.each do |task|
   Universe.create!(task)
 end
 
-jedis.each do |task|
-  Jedi.create!(task)
-end
 
 siths.each do |task|
   Sith.create!(task)
