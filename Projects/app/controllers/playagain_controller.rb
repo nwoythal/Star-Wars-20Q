ef50b95f4b56747278eb38969f8c623ba91554e7
Keanesean <Keanemsean@gmail.com>
2015-12-02 08:23:21 -0700

    Fixed gsub & split error

--- Projects/app/controllers/playagain_controller.rb
+++ Projects/app/controllers/playagain_controller.rb
@@ -1,8 +1,8 @@
 class PlayagainController < ApplicationController
   def index
-    @answer = params[:answer].gsub(/\\\./,'')
-    @guess = params[:guess].split.map(&:capitalize).join(' ')
-    @valid=@answer.downcase.gsub(/[^a-z0-9]/i,'').eql?(@guess.downcase.gsub(/[^a-z0-9]/i,'')) ? "Correct!" : "Incorrect!"
+    @answer = params[:answer].to_s.gsub(/\\\./,'')
+    @guess = params[:guess].to_s.split.map(&:capitalize).join(' ')
+    @valid=@answer.to_s.downcase.gsub(/[^a-z0-9]/i,'').eql?(@guess.to_s.downcase.gsub(/[^a-z0-9]/i,'')) ? "Correct!" : "Incorrect!"
     @pic = params[:pic]
   end
 end
