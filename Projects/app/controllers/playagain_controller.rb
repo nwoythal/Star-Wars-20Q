b46f061d19132a52fd4700aab03c17685ca782d5
nwoythal <nwoythal@gmail.com>
2015-12-02 01:19:38 -0700

    updated playagain so user input is validated

--- Projects/app/controllers/playagain_controller.rb
+++ Projects/app/controllers/playagain_controller.rb
@@ -1,8 +1,9 @@
 class PlayagainController < ApplicationController
   def index
-    @answer = params[:answer]
-    @guess = params[:guess]
-    @valid = params[:valid]
+    @answer = params[:answer].gsub(/\\\./,'')
+    @guess = params[:guess].split.map(&:capitalize).join(' ')
+    byebug
+    @valid=@answer.downcase.gsub(/[^a-z0-9]/i,'').eql?(@guess.downcase.gsub(/[^a-z0-9]/i,'')) ? "Correct!" : "Incorrect!"
     @pic = params[:pic]
   end
 end
