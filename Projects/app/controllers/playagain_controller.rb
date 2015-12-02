cbd38fcd0404eb459a9fef41e15b21ba59d324ef
nwoythal <nwoythal@gmail.com>
2015-12-02 01:23:37 -0700

    removed ebugger

--- Projects/app/controllers/playagain_controller.rb
+++ Projects/app/controllers/playagain_controller.rb
@@ -2,7 +2,6 @@ class PlayagainController < ApplicationController
   def index
     @answer = params[:answer].gsub(/\\\./,'')
     @guess = params[:guess].split.map(&:capitalize).join(' ')
-    byebug
     @valid=@answer.downcase.gsub(/[^a-z0-9]/i,'').eql?(@guess.downcase.gsub(/[^a-z0-9]/i,'')) ? "Correct!" : "Incorrect!"
     @pic = params[:pic]
   end
