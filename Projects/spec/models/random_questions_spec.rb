652916812861cc573ab565b1e39d57a7a21ae5c2
cshibe <cshibe@uccs.edu>
2015-12-02 12:02:55 -0700

    adding in tests

--- Projects/spec/models/random_questions_spec.rb
+++ Projects/spec/models/random_questions_spec.rb
@@ -1,7 +1,83 @@
 describe Bus do
   load "#{Rails.root}/db/seeds.rb"
   i = 0
-  while i <= 1000 do
+  while i <= 10000 do
+    Bus.grab_object_test2
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test3
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test4
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test5
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test6
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test7
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test8
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test9
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test10
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test11
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test12
+    i += 1
+  end
+  v
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test13
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test14
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test15
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
+    Bus.grab_object_test16
+    i += 1
+  end
+  i = 0
+  while i <= 10000 do
     Bus.grab_object
     i += 1
   end
